e28db95d44ca077ddc502e3fe3debb75d7463464
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-04-01 19:38:34 +0200

    feat: access to the archive with the images

---
+++ js/script.js
@@ -0,0 +1,89 @@
+// const slides = [
+// {
+//     image: 'img/01.webp',
+//         title: 'Marvel\'s Spiderman Miles Morale',
+//         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+//     }, {
+//         image: 'img/02.webp',
+//         title: 'Ratchet & Clank: Rift Apart',
+//         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+//     }, {
+//         image: 'img/03.webp',
+//         title: 'Fortnite',
+//         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+//     }, {
+//         image: 'img/04.webp',
+//         title: 'Stray',
+//         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+//     }, {
+//         image: 'img/05.webp',
+//         title: "Marvel's Avengers",
+//         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+//     }
+// ];
+
+// console.log(slides);
+
+// Partendo dal markup fornito nello starter pack, rifare lo slider ma questa volta usando Vue.
+// partiamo facendo scorrere le img con le freccie su e giu
+// collegare le img del thumbs con l'img principale
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                },
+                {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                },
+                {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                },
+                {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                },
+                {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ],
+           activeImage: 0,
+        };
+        
+    }
+}).mount('#app');
+
+
+
+
+// const {
+//     createApp
+//   } = Vue;
+//   createApp({
+  
+//     data() {
+//       return {
+        
+//       }
+//     },
+//     methods: {
+      
+//     },
+//     mounted() {
+      
+//   }).mount('#app')
+
+// const { createApp } = Vue;
